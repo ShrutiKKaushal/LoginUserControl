f3ed6d9f8beaf68ebb23643c07dcec5ce72fed2d
Shruti Kaushal <shruti_01@live.com>
2017-02-04 00:47:48 -0500

    LoginUserControl

---
+++ LoginApplication/LoginApplication/frmLogin.cs
@@ -0,0 +1,61 @@
+﻿/*Shruti Kaushal
+ *300804023
+ Comp 212-Assignment 2*/
+using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LoginApplication
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        //Connection String
+        
+        string cs = @"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\shrut\OneDrive\Documents\Centennial - 4th Sem\Comp212\Assignments\Comp212-Assignment2\LoginApplication\LoginApplication\MyDatabase.mdf;Integrated Security = True";
+        //btn_Submit Click event
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(txt_UserName.Text=="" || txt_Password.Text=="")
+            {
+                MessageBox.Show("Please provide UserName and Password");
+                return;
+            }
+            try
+            {
+                //Create SqlConnection
+                SqlConnection con = new SqlConnection(cs);
+                SqlCommand cmd = new SqlCommand("Select * from tbl_Login where UserName=@username and Password=@password",con);
+                cmd.Parameters.AddWithValue("@username",txt_UserName.Text);
+                cmd.Parameters.AddWithValue("@password", txt_Password.Text);
+                con.Open();
+                SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapt.Fill(ds);
+                con.Close();
+                int count = ds.Tables[0].Rows.Count;
+                //If count is equal to 1, than show frmMain form
+                if (count == 1)
+                {
+                    MessageBox.Show("Login Successful!");
+                    this.Hide();
+                    frmMain fm = new frmMain();
+                    fm.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Login Failed!");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ LoginApplication/LoginApplication/frmMain.Designer.cs
@@ -0,0 +1,77 @@
+﻿//Shruti Kaushal
+namespace LoginApplication
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_LogOut = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_LogOut
+            // 
+            this.btn_LogOut.Location = new System.Drawing.Point(528, 12);
+            this.btn_LogOut.Name = "btn_LogOut";
+            this.btn_LogOut.Size = new System.Drawing.Size(75, 23);
+            this.btn_LogOut.TabIndex = 0;
+            this.btn_LogOut.Text = "Log out";
+            this.btn_LogOut.UseVisualStyleBackColor = true;
+            this.btn_LogOut.Click += new System.EventHandler(this.btn_LogOut_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(173, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "You are Logged In..!!!";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 337);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_LogOut);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Application";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_LogOut;
+        private System.Windows.Forms.Label label1;
+    }
+}
